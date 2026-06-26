90eea26c1cdc3801214acd20b1d39e72380b6858
Anabelle Porras Carrillo <anabelle.porras@ucr.ac.cr>
2026-06-26 13:23:23 -0600

    optimizacion para que disminuir el rango de roll

--- Emergente.js
+++ Emergente.js
@@ -1,15 +1,16 @@
-
-// Contadores para los colores que lee de los vecinos
 var rojos = 0;
 var azules = 0;
 
-// Canales de transmision:
-// 0: rojo
-// 1: azul
-var channels = [0, 1];
+const ROJO = 0;
+const AZUL = 1;
+
+const VELOCIDAD = 35;
+const TIEMPO_MOVIMIENTO = 0.35;
+const TIEMPO_VOTACION = 0.7;
+const TIEMPO_RONDA = 0.2;
 
 function setColor(color) {
-    if (color === 0) {
+    if (color === ROJO) {
         setMainLed({ r: 255, g: 0, b: 0 });
     } else {
         setMainLed({ r: 0, g: 0, b: 255 });
@@ -17,61 +18,55 @@ function setColor(color) {
 }
 
 async function broadcastColor(color) {
-    if (color === 0) {
-        startIRBroadcast(4, 0);
-    } else {
-        startIRBroadcast(4, 1);
-    }
+    startIRBroadcast(4, color);
 }
 
 async function move() {
     while (true) {
         let direccion = Math.floor(Math.random() * 360);
-        await roll(direccion, 60); 
-        await delay(1.0); 
-        await delay(0.2);
+
+        await roll(direccion, VELOCIDAD, TIEMPO_MOVIMIENTO);
+        await stopRoll();
+
+        await delay(0.15);
     }
 }
 
 async function onIRMessage(channel) {
-  if (channel === 0) {
-    rojos++;
-  }
-
-  if (channel === 1) {
-    azules++;
-  }
+    if (channel === ROJO) {
+        rojos++;
+    } else if (channel === AZUL) {
+        azules++;
+    }
 }
 
 registerEvent(EventType.onIRMessage, onIRMessage);
 
 async function startProgram() {
     var color = Math.floor(Math.random() * 2);
+
     setColor(color);
+
+    listenForIRMessage(ROJO, AZUL);
+
     move();
 
-    //listenForIRMessage(channels);
-    listenForIRMessage(channels[0], channels[1]);
-
     while (true) {
-        await broadcastColor(color);
-
-        // Esperamos 1.5 segundos recolectando los votos de los vecinos
-        await delay(1.5);
-
-        // Votación de consenso
-        if (rojos > azules) {
-            color = 0;
-        } else if (azules > rojos) {
-            color = 1;
-        }
-        
-        setColor(color);
-        
-        // Limpieza de datos para la siguiente ronda
         rojos = 0;
         azules = 0;
-        
-        await delay(0.5);
+
+        await broadcastColor(color);
+
+        await delay(TIEMPO_VOTACION);
+
+        if (rojos > azules) {
+            color = ROJO;
+        } else if (azules > rojos) {
+            color = AZUL;
+        }
+
+        setColor(color);
+
+        await delay(TIEMPO_RONDA);
     }
 }
